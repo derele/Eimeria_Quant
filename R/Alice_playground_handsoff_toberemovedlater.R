ff4fe97a6bed77f77372124942ca665f43b626e6
Alice <alice.cam.balard@gmail.com>
2020-12-07 09:00:47 +0100

    correct analyses for time window dpi[3-10]

--- R/Alice_playground_handsoff_toberemovedlater.R
+++ R/Alice_playground_handsoff_toberemovedlater.R
@@ -39,6 +39,9 @@ myData$Mouse_genotype <- factor(
              "MMm_F1 (Bu-Pw)", "MMm_F0 (Bu-Bu)", "MMm_F0 (Pw-Pw)"))
 # dpi as numbers
 myData$dpi = as.numeric(as.character(myData$dpi))
+
+# time window, dpi 3 to dpi 10, were we have non null data
+myData <- myData[myData$dpi %in% 3:10,]
 #  -----------------
 
 # 1. Calculate maximum value & at which dpi it happens
@@ -49,8 +52,8 @@ datMaxOPG = myData %>% dplyr::group_by(EH_ID) %>%
 table(datMaxOPG$dpi)
 # 1.2 fecal DNA
 datMaxDNA = myData %>% group_by(EH_ID) %>%
-  dplyr::filter(Genome_copies_mean == max(Genome_copies_mean, na.rm = T)) %>%
-  dplyr::select(EH_ID, dpi, Genome_copies_mean, Mouse_genotype)%>% data.frame()
+  dplyr::filter(Genome_copies_gFaeces == max(Genome_copies_gFaeces, na.rm = T)) %>%
+  dplyr::select(EH_ID, dpi, Genome_copies_gFaeces, Mouse_genotype)%>% data.frame()
 table(datMaxDNA$dpi)
 # 1.3 relative weight loss
 datMaxWL = myData %>% group_by(EH_ID) %>%
@@ -59,13 +62,11 @@ datMaxWL = myData %>% group_by(EH_ID) %>%
 table(datMaxWL$dpi)
 
 datMaxALL = merge(merge(datMaxOPG[c("EH_ID", "OPG", "Mouse_genotype")], 
-                        datMaxDNA[c("EH_ID", "Genome_copies_mean", "Mouse_genotype")]),
+                        datMaxDNA[c("EH_ID", "Genome_copies_gFaeces", "Mouse_genotype")]),
                   datMaxWL[c("EH_ID", "weightloss", "Mouse_genotype")])
 
-### NB:  try with a window!!
-
 # plot overview
-p1 = ggplot(myData, aes(x= dpi, y = Genome_copies_mean)) + theme_bw() +
+p1 = ggplot(myData, aes(x= dpi, y = Genome_copies_gFaeces)) + theme_bw() +
   geom_line() +   facet_grid(.~EH_ID)
 p2 = ggplot(myData, aes(x= dpi, y = OPG)) + theme_bw() +
   geom_line() +   facet_grid(.~EH_ID)
@@ -80,29 +81,106 @@ grid.arrange(p1, p2, p3, nrow = 3)
 # Granger causality: DROPPED. For details, see "Appendix" part at the end of the code
 # ----------------------------
 
+# Q1b
+# Granger causality
+
+# https://towardsdatascience.com/fun-with-arma-var-and-granger-causality-6fdd29d8391c
+# test autocorrelation
+inds = unique(myData$EH_ID)
+par(mfrow=c(1,2))
+for (i in inds){
+  series = sdtWin[myData$EH_ID %in% i,]
+  series = series[order(series$dpi),"OPG"]
+  # replace NA per 0 
+  series[is.na(series)] = 0
+  #Store acf values
+  acf = acf(series, plot = F)
+  #plot the series using title, x & ylabels, use lines and a marker at yaxis=0
+  plot(series, main="Series-1", xlab="Time", ylab="Values")
+  lines(series)
+  abline(h=0, col="red")
+  #Plot acf values
+  plot(acf, main = as.character(i))
+}
+par(mfrow=c(1,1))
+
+series = myData[sdtWin$EH_ID %in% "LM0224", c("OPG", "dpi")]
+series = series[myData(series$dpi),]
+# Check if the ACF plot has no bands crossing the threshold boundary except 0th lag
+# (which is the correlation of the observation with itself).
+
+install.packages("tseries")
+library(tseries)
+# adf.test computes the Augmented Dickey-Fuller test for the null that x has a unit root.
+# Unit root tests are tests for stationarity in a time series. A time series has 
+# stationarity if a shift in time doesn't cause a change in the shape of the distribution;
+# unit roots are one cause for non-stationarity. These tests are known for having low 
+# statistical power
+inds = unique(myData$EH_ID)
+for (i in inds){
+  series = myData[myData$EH_ID %in% i,]
+  series = series[order(series$dpi),"OPG"]
+  # replace NA per 0 
+  series[is.na(series)] = 0
+  # perform test
+  print(c(i, round(adf.test(series)$p.value, 4)))
+}
+## rejection of stationary TS in 3/22 individuals
+
+for (i in inds){
+  series = sdt[sdt$EH_ID %in% i,]
+  series = series[order(series$dpi),"Genome_copies_mean"]
+  # replace NA per 0 
+  series[is.na(series)] = 0
+  # perform test
+  print(c(i, round(adf.test(series)$p.value, 4)))
+}
+## no rejection of stationary TS
+
+# https://en.wikipedia.org/wiki/Granger_causality
+# Better terms: "precedence", or, as Granger himself later claimed in 1977, "temporally related".
+# Rather than testing whether Y causes X, the Granger causality tests whether Y forecasts X.
+
+# https://www.statisticshowto.com/granger-causality/#:~:text=Granger%20causality%20is%20a%20%E2%80%9Cbottom,see%20if%20they%20are%20correlated.
+# Make sure your time series is stationary before proceeding. Data should be transformed to eliminate the possibility of autocorrelation. You should also make sure your model doesn’t have any unit roots, as these will skew the test results.
+# The basic steps for running the test are:
+# State the null hypothesis and alternate hypothesis. For example, y(t) does not Granger-cause x(t).
+# Choose the lags. This mostly depends on how much data you have available. 
+## One way to choose lags i and j is to run a model order test (i.e. use a model order selection method). It might be easier just to pick several values and run the Granger test several times to see if the results are the same for different lag levels. The results should not be sensitive to lags.
+# Find the f-value. Two equations can be used to find if βj = 0 for all lags j
+
+# If you have a large number of variables and lags, your F-test can lose power. An alternative would be to run a chi-square test, constructed with likelihood ratio or Wald tests. Although both versions give practically the same result, the F-test is much easier to run.
+
+library(plm)
+pgrangertest(OPG~Genome_copies_gFaeces, data=myData, index=c("EH_ID", "dpi"))
+
+pgrangertest(OPG~Genome_copies_gFaeces, data=myData, index=c("EH_ID", "dpi"))
+# Error in pgrangertest(OPG ~ Genome_copies_gFaeces, data = myData, index = c("EH_ID",  : 
+# Condition for test = "Ztilde" not met for all individuals: length of time series must be larger than 5+3*order (>5+3*1=8)
+
 # ----------------------------
 # Q2b: Does DNA predict a health effect on the host “overall” (e.g. maximum)?
 # ----------------------------
 
 # Extract metrics: # what is the peak day? # what is the peak value?
-ggplot(datMaxALL, aes(x = OPG, y = Genome_copies_mean)) +
+ggplot(datMaxALL, aes(x = OPG, y = Genome_copies_gFaeces)) +
   geom_point() + theme_bw()
-cor.test(datMaxALL$OPG, datMaxALL$Genome_copies_mean, method = "spearman")
+cor.test(datMaxALL$OPG, datMaxALL$Genome_copies_gFaeces, method = "spearman")
 
 ggplot(datMaxALL, aes(x = OPG, y = weightloss)) +
   geom_point() + theme_bw()
 cor.test(datMaxALL$OPG, datMaxALL$weightloss, method = "spearman")
 
-ggplot(datMaxALL, aes(x = Genome_copies_mean, y = weightloss)) +
+ggplot(datMaxALL, aes(x = Genome_copies_gFaeces, y = weightloss)) +
   geom_point() + theme_bw()
-cor.test(datMaxALL$Genome_copies_mean, datMaxALL$weightloss, method = "spearman")
+cor.test(datMaxALL$Genome_copies_gFaeces, datMaxALL$weightloss, method = "spearman")
 
 # Can we predict weight loss by a combination of OPG and fecDNA?
 library(lmtest)
-modFull = lm(weightloss ~ OPG * Genome_copies_mean, data = datMaxALL)
-modminusOPG = lm(weightloss ~ Genome_copies_mean, data = datMaxALL)
+modFull = lm(weightloss ~ OPG * Genome_copies_gFaeces, data = datMaxALL)
+modminusOPG = lm(weightloss ~ Genome_copies_gFaeces, data = datMaxALL)
 modminusDNA = lm(weightloss ~ OPG, data = datMaxALL)
-modminusInter = lm(weightloss ~ OPG + Genome_copies_mean, data = datMaxALL)
+modminusInter = lm(weightloss ~ OPG + Genome_copies_gFaeces, data = datMaxALL)
 list(signifOG = lrtest(modFull, modminusOPG),
      signifDNA = lrtest(modFull, modminusDNA),
      signifInter = lrtest(modFull, modminusInter))
@@ -118,8 +196,8 @@ testSignifWithinParas_updated <- function(dataframe, which){
     modFULL <- MASS::glm.nb(OPG ~ Mouse_genotype, data = dataframe)
     mod0 <- MASS::glm.nb(OPG ~ 1, data = dataframe)
   } else if (which == "RES_DNA"){
-    modFULL <- MASS::glm.nb(Genome_copies_mean ~ Mouse_genotype, data = dataframe)
-    mod0 <- MASS::glm.nb(Genome_copies_mean ~ 1, data = dataframe)
+    modFULL <- MASS::glm.nb(Genome_copies_gFaeces ~ Mouse_genotype, data = dataframe)
+    mod0 <- MASS::glm.nb(Genome_copies_gFaeces ~ 1, data = dataframe)
   } else if (which == "IMP"){
     modFULL <- lm(weightloss ~ Mouse_genotype, data = dataframe)
     mod0 <- lm(weightloss ~ 1, data = dataframe)
@@ -127,8 +205,8 @@ testSignifWithinParas_updated <- function(dataframe, which){
     modFULL <- lm(weightloss ~ 0 + OPG : Mouse_genotype, data = dataframe)
     mod0 <- lm(weightloss ~ 0 + OPG, data = dataframe)
   } else if (which == "TOL_DNA"){
-    modFULL <- lm(weightloss ~ 0 + Genome_copies_mean : Mouse_genotype, data = dataframe)
-    mod0 <- lm(weightloss ~ 0 + Genome_copies_mean, data = dataframe)
+    modFULL <- lm(weightloss ~ 0 + Genome_copies_gFaeces : Mouse_genotype, data = dataframe)
+    mod0 <- lm(weightloss ~ 0 + Genome_copies_gFaeces, data = dataframe)
   }
   G <- lrtest(modFULL, mod0)
   return(list(modfull = modFULL, LRT = G))
@@ -158,99 +236,9 @@ p2=ggplot(DFpredicted2, aes(x, predicted)) + geom_point() + theme_bw() +
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
 plot_grid(p1, p2, labels = c("A: OPG", "B: DNA"))
 
-
-
-
-
-
 # ------------------
 # APPENDIX (Junk...)
 # ------------------
-
-# Granger causality
-# https://towardsdatascience.com/fun-with-arma-var-and-granger-causality-6fdd29d8391c
-# test autocorrelation
-inds = unique(sdt$EH_ID)
-par(mfrow=c(1,2))
-for (i in inds){
-  series = sdt[sdt$EH_ID %in% i,]
-  series = series[order(series$dpi),"OPG"]
-  # replace NA per 0 
-  series[is.na(series)] = 0
-  #Store acf values
-  acf = acf(series, plot = F)
-  #plot the series using title, x & ylabels, use lines and a marker at yaxis=0
-  plot(series, main="Series-1", xlab="Time", ylab="Values")
-  lines(series)
-  abline(h=0, col="red")
-  #Plot acf values
-  plot(acf, main = as.character(i))
-}
-par(mfrow=c(1,1))
-
-series = sdt[sdt$EH_ID %in% "LM0224", c("OPG", "dpi")]
-series = series[order(series$dpi),]
-
-# Check if the ACF plot has no bands crossing the threshold boundary except 0th lag
-# (which is the correlation of the observation with itself).
-
-install.packages("tseries")
-library(tseries)
-# adf.test computes the Augmented Dickey-Fuller test for the null that x has a unit root.
-# Unit root tests are tests for stationarity in a time series. A time series has 
-# stationarity if a shift in time doesn't cause a change in the shape of the distribution;
-# unit roots are one cause for non-stationarity. These tests are known for having low 
-# statistical power
-inds = unique(sdt$EH_ID)
-for (i in inds){
-  series = sdt[sdt$EH_ID %in% i,]
-  series = series[order(series$dpi),"OPG"]
-  # replace NA per 0 
-  series[is.na(series)] = 0
-  # perform test
-  print(c(i, round(adf.test(series)$p.value, 4)))
-}
-## rejection of stationary TS in 3/22 individuals
-
-for (i in inds){
-  series = sdt[sdt$EH_ID %in% i,]
-  series = series[order(series$dpi),"Genome_copies_mean"]
-  # replace NA per 0 
-  series[is.na(series)] = 0
-  # perform test
-  print(c(i, round(adf.test(series)$p.value, 4)))
-}
-## rejection of stationary TS in 7/22
-
-# https://en.wikipedia.org/wiki/Granger_causality
-# Better terms: "precedence", or, as Granger himself later claimed in 1977, "temporally related".
-# Rather than testing whether Y causes X, the Granger causality tests whether Y forecasts X.
-
-# https://www.statisticshowto.com/granger-causality/#:~:text=Granger%20causality%20is%20a%20%E2%80%9Cbottom,see%20if%20they%20are%20correlated.
-# Make sure your time series is stationary before proceeding. Data should be transformed to eliminate the possibility of autocorrelation. You should also make sure your model doesn’t have any unit roots, as these will skew the test results.
-# The basic steps for running the test are:
-# State the null hypothesis and alternate hypothesis. For example, y(t) does not Granger-cause x(t).
-# Choose the lags. This mostly depends on how much data you have available. 
-## One way to choose lags i and j is to run a model order test (i.e. use a model order selection method). It might be easier just to pick several values and run the Granger test several times to see if the results are the same for different lag levels. The results should not be sensitive to lags.
-# Find the f-value. Two equations can be used to find if βj = 0 for all lags j
-
-# If you have a large number of variables and lags, your F-test can lose power. An alternative would be to run a chi-square test, constructed with likelihood ratio or Wald tests. Although both versions give practically the same result, the F-test is much easier to run.
-
-library(plm)
-pgrangertest(OPG~Genome_copies_mean, data=sdt, index=c("EH_ID", "dpi"))
-
-# hypothesis: DNA does not cause OPG for all individuals
-# Susie: Granger causality
-# hypothesis: DNA does not cause OPG for all individuals
-# pgrangertest(OPG~Genome_copies_mean, data=mytab, index=c("EH_ID", "dpi"))
-# 
-# Panel Granger (Non-)Causality Test (Dumitrescu/Hurlin (2012))                                                                                                                                     
-# 
-# data:  OPG ~ Genome_copies_mean                                                                                                                                                                           
-# Ztilde = 93.114, p-value < 2.2e-16                                                                                                                                                                        
-# alternative hypothesis: Granger causality for at least one individual 
-
-
 
 # A simple method for distinguishing within- versus between-subjecteffects using 
 # mixed modelsMartijn van de Pol*, Jonathan Wright
