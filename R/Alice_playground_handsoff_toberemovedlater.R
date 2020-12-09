af9648bdaa24ac1a3e385a84063a3b33e93d90d6
Alice <alice.cam.balard@gmail.com>
2020-12-09 20:38:29 +0100

    remove figures of logreg fit - failed

--- R/Alice_playground_handsoff_toberemovedlater.R
+++ R/Alice_playground_handsoff_toberemovedlater.R
@@ -59,178 +59,57 @@ datMaxWL = myData %>% group_by(EH_ID) %>%
   dplyr::select(EH_ID, dpi, weightloss, Mouse_genotype)%>% data.frame()
 table(datMaxWL$dpi)
 
-datMaxALL1 = merge(merge(datMaxOPG[c("EH_ID", "OPG", "Mouse_genotype")], 
+datMaxALL = merge(merge(datMaxOPG[c("EH_ID", "OPG", "Mouse_genotype")], 
                         datMaxDNA[c("EH_ID", "Genome_copies_gFaeces", "Mouse_genotype")]),
                   datMaxWL[c("EH_ID", "weightloss", "Mouse_genotype")])
 
 # ----------------------------
-# NB. NOW WITH TIME WINDOW
-myData0 = myData # keep full for comparison
-# time window, dpi 3 to dpi 10, were we have non null data
-myData <- myData[myData$dpi %in% 3:10,]
-# ----------------------------
-
-# 1. Calculate maximum value & at which dpi it happens
-# 1.1 OPG
-datMaxOPG = myData %>% dplyr::group_by(EH_ID) %>%
-  filter(OPG == max(OPG, na.rm = T)) %>%
-  dplyr::select(EH_ID, dpi, OPG, Mouse_genotype)%>% data.frame()
-table(datMaxOPG$dpi)
-# 1.2 fecal DNA
-datMaxDNA = myData %>% group_by(EH_ID) %>%
-  dplyr::filter(Genome_copies_gFaeces == max(Genome_copies_gFaeces, na.rm = T)) %>%
-  dplyr::select(EH_ID, dpi, Genome_copies_gFaeces, Mouse_genotype)%>% data.frame()
-table(datMaxDNA$dpi)
-# 1.3 relative weight loss
-datMaxWL = myData %>% group_by(EH_ID) %>%
-  filter(weightloss == max(weightloss, na.rm = T)) %>%
-  dplyr::select(EH_ID, dpi, weightloss, Mouse_genotype)%>% data.frame()
-table(datMaxWL$dpi)
-
-datMaxALL2 = merge(merge(datMaxOPG[c("EH_ID", "OPG", "Mouse_genotype")], 
-                         datMaxDNA[c("EH_ID", "Genome_copies_gFaeces", "Mouse_genotype")]),
-                   datMaxWL[c("EH_ID", "weightloss", "Mouse_genotype")])
-
-# Comparison with/without window
-comp = merge(datMaxALL1, datMaxALL2, by = c("EH_ID", "Mouse_genotype"))
-
-table(comp$OPG.x != comp$OPG.y) # does not change OPG results
-table(comp$Genome_copies_gFaeces.x != comp$Genome_copies_gFaeces.y) # does not change DNA results
-table(comp$weightloss.x != comp$weightloss.y) # change for 2 animals weight loss
-
-comp[comp$weightloss.x != comp$weightloss.y,]
-
-check = myData0[myData0$EH_ID %in% c("LM0210", "LM0219"),]
-ggplot(check, aes(x = dpi, y = weight, col =EH_ID)) + geom_line() + geom_point()
-
-# ----------------------------
-### Q1: Is the DNA coming from the counted oocyst?
-# Q1a: Correlation of the two (Susie's code)
-# Q1b: Prediction of one b the other with a lag?
-# Granger causality: DROPPED. For details, see "Appendix" part at the end of the code
-# ----------------------------
-
-# Q1b
-# Granger causality
-
-# https://towardsdatascience.com/fun-with-arma-var-and-granger-causality-6fdd29d8391c
-# test autocorrelation
-inds = unique(myData$EH_ID)
-par(mfrow=c(1,2))
-for (i in inds){
-  series = sdtWin[myData$EH_ID %in% i,]
-  series = series[order(series$dpi),"OPG"]
-  # replace NA per 0 
-  series[is.na(series)] = 0
-  #Store acf values
-  acf = acf(series, plot = F)
-  #plot the series using title, x & ylabels, use lines and a marker at yaxis=0
-  plot(series, main="Series-1", xlab="Time", ylab="Values")
-  lines(series)
-  abline(h=0, col="red")
-  #Plot acf values
-  plot(acf, main = as.character(i))
-}
-par(mfrow=c(1,1))
-
-series = myData[sdtWin$EH_ID %in% "LM0224", c("OPG", "dpi")]
-series = series[myData(series$dpi),]
-# Check if the ACF plot has no bands crossing the threshold boundary except 0th lag
-# (which is the correlation of the observation with itself).
-
-# install.packages("tseries")
-# library(tseries)
-# adf.test computes the Augmented Dickey-Fuller test for the null that x has a unit root.
-# Unit root tests are tests for stationarity in a time series. A time series has 
-# stationarity if a shift in time doesn't cause a change in the shape of the distribution;
-# unit roots are one cause for non-stationarity. These tests are known for having low 
-# statistical power
-# inds = unique(myData$EH_ID)
-# for (i in inds){
-#   series = myData[myData$EH_ID %in% i,]
-#   series = series[order(series$dpi),"OPG"]
-#   # replace NA per 0 
-#   series[is.na(series)] = 0
-#   # perform test
-#   print(c(i, round(adf.test(series)$p.value, 4)))
-# }
-## rejection of stationary TS in 3/22 individuals
-
-# for (i in inds){
-#   series = sdt[sdt$EH_ID %in% i,]
-#   series = series[order(series$dpi),"Genome_copies_mean"]
-#   # replace NA per 0 
-#   series[is.na(series)] = 0
-#   # perform test
-#   print(c(i, round(adf.test(series)$p.value, 4)))
-# }
-## no rejection of stationary TS
-
-# https://en.wikipedia.org/wiki/Granger_causality
-# Better terms: "precedence", or, as Granger himself later claimed in 1977, "temporally related".
-# Rather than testing whether Y causes X, the Granger causality tests whether Y forecasts X.
-
-# https://www.statisticshowto.com/granger-causality/#:~:text=Granger%20causality%20is%20a%20%E2%80%9Cbottom,see%20if%20they%20are%20correlated.
-# Make sure your time series is stationary before proceeding. Data should be transformed to eliminate the possibility of autocorrelation. You should also make sure your model doesn’t have any unit roots, as these will skew the test results.
-# The basic steps for running the test are:
-# State the null hypothesis and alternate hypothesis. For example, y(t) does not Granger-cause x(t).
-# Choose the lags. This mostly depends on how much data you have available. 
-## One way to choose lags i and j is to run a model order test (i.e. use a model order selection method). It might be easier just to pick several values and run the Granger test several times to see if the results are the same for different lag levels. The results should not be sensitive to lags.
-# Find the f-value. Two equations can be used to find if βj = 0 for all lags j
-
-# If you have a large number of variables and lags, your F-test can lose power. An alternative would be to run a chi-square test, constructed with likelihood ratio or Wald tests. Although both versions give practically the same result, the F-test is much easier to run.
-
-library(plm)
-pgrangertest(OPG~Genome_copies_gFaeces, data=myData, index=c("EH_ID", "dpi"))
-
-pgrangertest(OPG~Genome_copies_gFaeces, data=myData, index=c("EH_ID", "dpi"))
-# Error in pgrangertest(OPG ~ Genome_copies_gFaeces, data = myData, index = c("EH_ID",  : 
-# Condition for test = "Ztilde" not met for all individuals: length of time series must be larger than 5+3*order (>5+3*1=8)
-
-# ----------------------------
-# Q2b: Does DNA predict a health effect on the host “overall” (e.g. maximum)?
+# Q2(a): Does DNA predict a health effect on the host “overall” (e.g. maximum)?
 # ----------------------------
 
 # Extract metrics: # what is the peak day? # what is the peak value?
-ggplot(datMaxALL1, aes(x = OPG, y = Genome_copies_gFaeces)) +
+ggplot(datMaxALL, aes(x = OPG, y = Genome_copies_gFaeces)) +
   geom_point() + theme_bw()
-cor.test(datMaxALL1$OPG, datMaxALL1$Genome_copies_gFaeces, method = "spearman")
-# on window: does not change results
-ggplot(datMaxALL2, aes(x = OPG, y = Genome_copies_gFaeces)) +
-  geom_point() + theme_bw()
-cor.test(datMaxALL2$OPG, datMaxALL2$Genome_copies_gFaeces, method = "spearman")
+cor.test(datMaxALL$OPG, datMaxALL$Genome_copies_gFaeces, method = "spearman")
 
-ggplot(datMaxALL1, aes(x = OPG, y = weightloss)) +
+ggplot(datMaxALL, aes(x = OPG, y = weightloss)) +
   geom_point() + theme_bw()
-cor.test(datMaxALL1$OPG, datMaxALL1$weightloss, method = "spearman")
-# on window: change results but not conclusion
-ggplot(datMaxALL2, aes(x = OPG, y = weightloss)) +
-  geom_point() + theme_bw()
-cor.test(datMaxALL2$OPG, datMaxALL2$weightloss, method = "spearman")
+cor.test(datMaxALL$OPG, datMaxALL$weightloss, method = "spearman")
 
-ggplot(datMaxALL1, aes(x = Genome_copies_gFaeces, y = weightloss)) +
+ggplot(datMaxALL, aes(x = Genome_copies_gFaeces, y = weightloss)) +
   geom_point() + theme_bw()
-cor.test(datMaxALL1$Genome_copies_gFaeces, datMaxALL1$weightloss, method = "spearman")
-# on window: change results but not conclusion
-ggplot(datMaxALL2, aes(x = Genome_copies_gFaeces, y = weightloss)) +
-  geom_point() + theme_bw()
-cor.test(datMaxALL2$Genome_copies_gFaeces, datMaxALL2$weightloss, method = "spearman")
+cor.test(datMaxALL$Genome_copies_gFaeces, datMaxALL$weightloss, method = "spearman")
 
 # Can we predict weight loss by a combination of OPG and fecDNA?
 library(lmtest)
-modFull = lm(weightloss ~ OPG * Genome_copies_gFaeces, data = datMaxALL1)
-modminusOPG = lm(weightloss ~ Genome_copies_gFaeces, data = datMaxALL1)
-modminusDNA = lm(weightloss ~ OPG, data = datMaxALL1)
-modminusInter = lm(weightloss ~ OPG + Genome_copies_gFaeces, data = datMaxALL1)
+modFull = lm(weightloss ~ OPG * Genome_copies_gFaeces, data = datMaxALL)
+modminusOPG = lm(weightloss ~ Genome_copies_gFaeces, data = datMaxALL)
+modminusDNA = lm(weightloss ~ OPG, data = datMaxALL)
+modminusInter = lm(weightloss ~ OPG + Genome_copies_gFaeces, data = datMaxALL)
 list(signifOG = lrtest(modFull, modminusOPG),
      signifDNA = lrtest(modFull, modminusDNA),
      signifInter = lrtest(modFull, modminusInter))
+
+summary(modFull)
+summary(modminusInter)
+
+par(mfrow= c(2,2))
+plot(modFull) # saved as FigS_modelFit_alice.pdf
+par(mfrow= c(1,1))
+
+
+library(ggeffects)
+ggpredict(modFull)
+
+
 # Results:
 # We tested if the maximum weight loss during the experiment was influenced by OPG, 
 # Genome_copies_gFaeces and their interaction. To test the significance of the marginal 
 # contribution of each parameter to the full model, each parameter was removed from 
 # the full model, and the difference between full and reduced model was assessed using 
 # likelihood ratio tests (G). 
+
+
   
 # We found that the maximum weight loss during infection was influenced by
 # both OPG and fecal Eimeria DNA; Their interaction was not found significant
